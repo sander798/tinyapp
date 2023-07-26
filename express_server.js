d160658bdbb0f972acf974073b141bf6b9c46e09
Alexander Evans <sander7989@gmail.com>
2023-07-26 12:55:21 -0600

    add server logic for recieving new url

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const crypto = require("crypto");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return crypto.randomBytes(3).toString("hex");
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,6 +31,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
