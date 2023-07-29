9ddcadfefd9a0183d235972b27eea58472dc9ca6
Alexander Evans <sander7989@gmail.com>
2023-07-29 11:17:30 -0600

    redirect to /login on logout

--- express_server.js
+++ express_server.js
@@ -151,7 +151,7 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 app.listen(PORT, () => {
