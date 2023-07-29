b06e899626e98f6fc8ccf5f0aa0ca778d8a8067f
Alexander Evans <sander7989@gmail.com>
2023-07-28 21:30:18 -0600

    add /register endpoint

--- express_server.js
+++ express_server.js
@@ -79,6 +79,16 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"], };
+  res.render("register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  //res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
