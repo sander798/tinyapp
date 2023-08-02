5ceac0f379a9f6a892ca2d08019bd23a7fa510b3
Alexander Evans <sander7989@gmail.com>
2023-08-01 18:33:46 -0600

    add file for holding data objects

---
+++ data.js
@@ -0,0 +1,24 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "testUser",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "testUser",
+  },
+};
+
+const users = {
+  testUser: {
+    id: "testUser",
+    email: "testUser@test.com",
+    password: "$2a$10$Q3T742NMxmons95v9Zv.W.LbXxRattl65lWYjPdZGi9VoT0PuAKGK",
+    //correctbatteryhorsestaple
+  },
+};
+
+module.exports = {
+  urlDatabase,
+  users,
+};
